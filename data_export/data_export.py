f088da358f9333c3f49dedf3506f65a0fe44828b
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-01 13:59:13 +0100

    file skeleton

--- data_export/data_export.py
+++ data_export/data_export.py
@@ -1,3 +1,10 @@
+import requests
+
+HEADERS = {'X-ClientId': '7cdd07b1-a88d-46ab-aace-08d87d719481'}
+
+# Docs:
+# https://api.stat.gov.pl/Home/BdlApi
+
 # links to variables are here, take all one call sufficient
 # https://bdl.stat.gov.pl/api/v1/variables?subject-id=P2759&format=json&page-size=100
     
@@ -5,5 +12,11 @@
 # these will have more than one page
 # https://bdl.stat.gov.pl/api/v1/data/by-variable/148095?format=json&page=0&page-size=100
 
-API_KEY = '7cdd07b1-a88d-46ab-aace-08d87d719481'
-# https://api.stat.gov.pl/Home/BdlApi
+def main():
+    """
+    data export
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
