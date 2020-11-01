5cec9e332e044a7646b45c3ab0c7211e403d1564
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-01 13:53:24 +0100

    init pyfile

---
+++ data_export/data_export.py
@@ -0,0 +1,9 @@
+# links to variables are here, take all one call sufficient
+# https://bdl.stat.gov.pl/api/v1/variables?subject-id=P2759&format=json&page-size=100
+    
+# loop through all ids e.g. 148074
+# these will have more than one page
+# https://bdl.stat.gov.pl/api/v1/data/by-variable/148095?format=json&page=0&page-size=100
+
+API_KEY = '7cdd07b1-a88d-46ab-aace-08d87d719481'
+# https://api.stat.gov.pl/Home/BdlApi
