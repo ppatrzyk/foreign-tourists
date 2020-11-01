c7c0efa04f64381735cb47547267bce0d6dbec82
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-01 14:41:51 +0100

    country get data func

--- data_export/data_export.py
+++ data_export/data_export.py
@@ -1,10 +1,12 @@
 import requests
 import json
+from math import ceil
 
 HEADERS = {'X-ClientId': '7cdd07b1-a88d-46ab-aace-08d87d719481'}
+PAGE_SIZE = 100
 FORMAT_PARAMS = {
     'format': 'json',
-    'page-size': 100,
+    'page-size': PAGE_SIZE,
 }
 
 # Docs:
@@ -34,17 +36,32 @@ def get_country_data(variable_id):
     get data for one country (country=variable_id)
     all powiats/years returned here
     """
-    data = {}
-    return data
+    url = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{variable_id}'
+    response = requests.get(url, params=FORMAT_PARAMS, headers=HEADERS)
+    total = response.json().get('totalRecords')
+    pages = ceil(total / PAGE_SIZE)
+    country_data = []
+    for page in range(pages):
+        params = {
+            **FORMAT_PARAMS,
+            'page': page,
+        }
+        response = requests.get(url, params=params, headers=HEADERS)
+        country_data.append(response.get('results'))
+    return country_data
 
 def main():
     """
     data export
     """
     country_ids = get_country_ids()
+    all_data = dict()
     for variable_id, country in country_ids.items:
         country_data = get_country_data(variable_id)
-    # print(json.dumps(data, indent=4))
+        all_data[country] = country_data
+    with open('data_export/raw_data.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(all_data))
+    return True
 
 if __name__ == "__main__":
     main()
