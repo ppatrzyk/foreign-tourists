1c5ecc2520c09f08d3c30ea8ef3dde588ca75c8b
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-01 14:12:19 +0100

    init var list

--- data_export/data_export.py
+++ data_export/data_export.py
@@ -1,6 +1,11 @@
 import requests
+import json
 
 HEADERS = {'X-ClientId': '7cdd07b1-a88d-46ab-aace-08d87d719481'}
+FORMAT_PARAMS = {
+    'format': 'json',
+    'page-size': 100,
+}
 
 # Docs:
 # https://api.stat.gov.pl/Home/BdlApi
@@ -12,11 +17,25 @@ HEADERS = {'X-ClientId': '7cdd07b1-a88d-46ab-aace-08d87d719481'}
 # these will have more than one page
 # https://bdl.stat.gov.pl/api/v1/data/by-variable/148095?format=json&page=0&page-size=100
 
+def get_variables():
+    """
+    list of variables from given 'topic'
+    """
+    url = 'https://bdl.stat.gov.pl/api/v1/variables'
+    params = {
+        **FORMAT_PARAMS,
+        'subject-id': 'P2759',
+    }
+    result = requests.get(url, params=params, headers=HEADERS)
+    data = result.json()
+    return data
+
 def main():
     """
     data export
     """
-    pass
+    data = get_variables()
+    print(json.dumps(data, indent=4))
 
 if __name__ == "__main__":
     main()
