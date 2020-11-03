785ae00bbaeb010ce5f9f7efb65a72d984c6031c
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-03 17:03:16 +0100

    reformat to list of dicts

--- data_export/data_reformat.py
+++ data_export/data_reformat.py
@@ -3,7 +3,10 @@ import json
 def parse_entry(entry):
     """
     """
-    data = tuple({'region': entry.get('name'), 'year': el.get('year'), 'count': el.get('val')} for el in entry.get('values'))
+    assert isinstance(entry, dict), entry
+    values = entry.get('values')
+    assert isinstance(values, list), values
+    data = tuple({'region': entry.get('name'), 'year': el.get('year'), 'count': el.get('val')} for el in values)
     return data
 
 def main():
@@ -12,7 +15,19 @@ def main():
     """
     with open('data_export/raw_data.json', 'r') as f:
         raw_data = json.loads(f.read())
-    print(raw_data.keys())
+    formattted_data = []
+    for country, data in raw_data.items():
+        print(country)
+        for country_outer in data:
+            for el in country_outer:
+                try:
+                    parsed_entries = parse_entry(el)
+                except:
+                    print(f'ERROR: {el}')
+                    continue
+                entries = tuple({'country': country, **el} for el in parsed_entries)
+                formattted_data.extend(entries)
+    print(len(formattted_data))
 
 if __name__ == "__main__":
     main()
