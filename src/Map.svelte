814ebf6b5c4dc77c4bf12bda1e873c0400cbf4ec
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-05 21:37:19 +0100

    data loading with json

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,5 +1,8 @@
 <script>
-	
+	import { json } from 'd3-fetch';
+
+	let country_codes;
+	json('data/country_codes.json').then(data => {country_codes = data; console.log(country_codes)});
 </script>
 
 <main>
