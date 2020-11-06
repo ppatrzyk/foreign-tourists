be29e15839f4c01292553ebc2149645260dfb9bc
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-06 18:05:21 +0100

    working store subscription

--- src/Map.svelte
+++ src/Map.svelte
@@ -2,13 +2,15 @@
 	import { json } from 'd3-fetch';
 	import app_state from './stores.js';
 
+	let state;
+	app_state.subscribe(app_state => state = app_state);
 	let country_codes;
 	json('data/country_codes.json').then(data => {country_codes = data; console.log(country_codes)});
 </script>
 
 <main>
 	<div>map placeholder</div>
-	<div>{JSON.stringify(app_state)}</div>
+	<div>{JSON.stringify(state)}</div>
 </main>
 
 <style>
--- src/stores.js
+++ src/stores.js
@@ -2,10 +2,11 @@
 import { writable } from "svelte/store";
 
 const init_data = writable({
-    'key': 'testval'
+    key: 'testval'
 });
 const app_state = {
     subscribe: init_data.subscribe
-}
+};
+
 
 export default app_state;
