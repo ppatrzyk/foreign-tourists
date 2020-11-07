0fbebd47dd570b325f2dc9deb06e881be52d59c9
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-07 15:41:09 +0100

    simplify drawing

--- src/Map.svelte
+++ src/Map.svelte
@@ -7,20 +7,14 @@
 
 	const MAP_WIDTH = 600;
 	const MAP_HEIGHT = 600;
-	const MAP_MARGIN = {top: 25, right: 25, bottom: 25, left: 25};
 
 	function draw_map(data) {
-		console.log(data)
 		var projection = geoMercator()
 			.fitSize([MAP_WIDTH, MAP_HEIGHT], data);
 
 		var path = geoPath().projection(projection);
 
-		var map = select("#map")
-			.append("svg")
-			.attr("width", MAP_WIDTH + MAP_MARGIN.left + MAP_MARGIN.right)
-			.attr("height", MAP_HEIGHT + MAP_MARGIN.top + MAP_MARGIN.bottom)
-			.attr("class", "border")
+		var map = select("#map");
 
 		map.selectAll("path")
 			.data(map_geojson.features)
@@ -29,17 +23,16 @@
 			.attr("d", path)
 			.attr("class", "country-border")
 	}
+	$: map_trigger = draw_map(map_geojson);
 </script>
 
 <div>
-	<div id="map"></div>
+	<div >
+		<svg id="map" width={MAP_WIDTH} height={MAP_HEIGHT}></svg>
+	</div>
 	<div>{JSON.stringify($app_state)}</div>
-	<div>{draw_map(map_geojson)}</div>
 </div>
 
 <style>
-	/* .country-border {
-		stroke: white;
-		stroke-width: 0.5;
-	} */
+
 </style>
