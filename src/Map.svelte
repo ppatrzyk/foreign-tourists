01f4d207a86efb1480d0b9baa3d185d0b3155d89
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-07 11:58:27 +0100

    fetch await to load data

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,14 +1,17 @@
 <script>
-	import { json } from 'd3-fetch';
 	import app_state from './stores.js';
 
-	let country_codes;
-	json('data/country_codes.json').then(data => {country_codes = data; console.log(country_codes)});
+	let country_codes = fetch('data/country_codes.json').then(res => res.json())
 </script>
 
 <main>
 	<div>map placeholder</div>
 	<div>{JSON.stringify($app_state)}</div>
+	{#await country_codes}
+		<p>waiting for the promise to resolve...</p>
+	{:then country_codes}
+		<p>The value is {JSON.stringify(country_codes)}</p>
+	{/await}
 </main>
 
 <style>
