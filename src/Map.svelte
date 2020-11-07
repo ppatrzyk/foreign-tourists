52de85cd52a37fc67de3697573634ddaa73bf666
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-07 12:46:46 +0100

    proper onmount workflow

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,17 +1,26 @@
 <script>
 	import app_state from './stores.js';
+	import { onMount } from 'svelte';
 
-	let country_codes = fetch('data/country_codes.json').then(res => res.json())
+	let country_codes = {};
+	let map_geojson = {};
+	onMount(async () => {
+		const country_codes_raw = await fetch('data/country_codes.json');
+		country_codes = await country_codes_raw.json();
+		const map_geojson_raw = await fetch('data/wojewodztwa-min.geojson');
+		map_geojson = await map_geojson_raw.json();
+	});
 </script>
 
 <main>
 	<div>map placeholder</div>
 	<div>{JSON.stringify($app_state)}</div>
-	{#await country_codes}
+	{#if Object.keys(country_codes).length === 0 & Object.keys(map_geojson).length === 0}
 		<p>waiting for the promise to resolve...</p>
-	{:then country_codes}
-		<p>The value is {JSON.stringify(country_codes)}</p>
-	{/await}
+	{:else}
+		{JSON.stringify(country_codes)}
+		{JSON.stringify(map_geojson)}
+	{/if}
 </main>
 
 <style>
