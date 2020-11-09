6b9e019a043a863e371e4086a0014b0ab322a76e
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-09 20:21:19 +0100

    language setting

--- src/Control.svelte
+++ src/Control.svelte
@@ -10,16 +10,15 @@
 <div>
     <h2>Control panel</h2>
     <p>{JSON.stringify($app_state)}</p>
-    <!-- <button on:click="{app_state.set_variable('country', country)}">{country}</button> -->
     <h3>Lang</h3>
-    <select bind:value={selected_lang}>
+    <!-- svelte-ignore a11y-no-onchange -->
+    <select bind:value={selected_lang} on:change="{app_state.set_variable('language', selected_lang.id)}">
 		{#each languages as language}
 			<option value={language}>
 				{language.text}
 			</option>
 		{/each}
     </select>
-    <p>{JSON.stringify(selected_lang)}</p>
 </div>
 
 <style>
